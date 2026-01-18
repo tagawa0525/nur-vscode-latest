07ab395842848e6d401fc37692ee904e2b8e6cae
Hiroaki Tagawa <tagawa0525@gmail.com>
2026-01-18 18:20:10 +0900

    feat: initial commit with VSCode 1.108.1

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "tagawa's NUR packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        import ./default.nix {
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        }
+      );
+    };
+}
---
+++ pkgs/vscode/default.nix
@@ -0,0 +1,124 @@
+{ lib
+, stdenv
+, fetchurl
+, makeWrapper
+, autoPatchelfHook
+, wrapGAppsHook3
+, alsa-lib
+, at-spi2-atk
+, cairo
+, cups
+, dbus
+, expat
+, fontconfig
+, freetype
+, gdk-pixbuf
+, glib
+, gtk3
+, libdrm
+, libnotify
+, libpulseaudio
+, libsecret
+, libuuid
+, libxkbcommon
+, mesa
+, nspr
+, nss
+, pango
+, systemd
+, xorg
+, libkrb5
+}:
+
+let
+  # VSCode最新版のメタデータを取得するスクリプト
+  # Microsoft公式APIから最新のstableバージョン情報を取得
+  inherit (stdenv.hostPlatform) system;
+  plat = {
+    x86_64-linux = "linux-x64";
+    aarch64-linux = "linux-arm64";
+    armv7l-linux = "linux-armhf";
+  }.${system};
+
+  # 最新版の情報（GitHub Actionsで自動更新される）
+  version = "1.108.1";
+  sha256 = "1dsvrf384qy3jfkwgkc7l0z3kyk17gw3v0rcb2gkx832k64n32x9";
+
+in
+stdenv.mkDerivation rec {
+  pname = "vscode";
+  inherit version;
+
+  src = fetchurl {
+    name = "vscode-${version}.tar.gz";
+    url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+    inherit sha256;
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+    autoPatchelfHook
+    wrapGAppsHook3
+  ];
+
+  buildInputs = [
+    alsa-lib
+    at-spi2-atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libdrm
+    libnotify
+    libpulseaudio
+    libsecret
+    libuuid
+    libxkbcommon
+    mesa
+    nspr
+    nss
+    pango
+    stdenv.cc.cc.lib
+    systemd
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libxcb
+    xorg.libxkbfile
+    xorg.libxshmfence
+    libkrb5
+  ];
+
+  runtimeDependencies = [
+    (lib.getLib systemd)
+  ];
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/vscode $out/bin
+    cp -r ./* $out/lib/vscode
+    ln -s $out/lib/vscode/bin/code $out/bin/code
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Visual Studio Code - latest version";
+    homepage = "https://code.visualstudio.com/";
+    license = licenses.unfree;
+    platforms = [ "x86_64-linux" "aarch64-linux" "armv7l-linux" ];
+    mainProgram = "code";
+  };
+}
